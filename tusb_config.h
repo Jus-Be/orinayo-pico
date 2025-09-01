30153de7f37fbb94e8af4d7215d894b9f9e20a22
Dele Olajide <dele@4ng.net>
2025-09-01 16:47:51 +0100

    use correct file

--- tusb_config.h
+++ tusb_config.h
@@ -30,20 +30,6 @@
 extern "C" {
 #endif
 
-//--------------------------------------------------------------------+
-// Board Specific Configuration
-//--------------------------------------------------------------------+
-
-// RHPort number used for device can be defined by board.mk, default to port 0
-#ifndef BOARD_TUD_RHPORT
-#define BOARD_TUD_RHPORT 0
-#endif
-
-// RHPort max operational speed can defined by board.mk
-#ifndef BOARD_TUD_MAX_SPEED
-#define BOARD_TUD_MAX_SPEED OPT_MODE_DEFAULT_SPEED
-#endif
-
 //--------------------------------------------------------------------
 // COMMON CONFIGURATION
 //--------------------------------------------------------------------
@@ -53,19 +39,24 @@ extern "C" {
 #error CFG_TUSB_MCU must be defined
 #endif
 
-#ifndef CFG_TUSB_OS
-#define CFG_TUSB_OS OPT_OS_NONE
+#if CFG_TUSB_MCU == OPT_MCU_LPC18XX || CFG_TUSB_MCU == OPT_MCU_LPC43XX || CFG_TUSB_MCU == OPT_MCU_MIMXRT10XX || \
+    CFG_TUSB_MCU == OPT_MCU_NUC505 || CFG_TUSB_MCU == OPT_MCU_CXD56
+#define CFG_TUSB_RHPORT0_MODE     (OPT_MODE_DEVICE | OPT_MODE_HIGH_SPEED)
+#else
+#define CFG_TUSB_RHPORT0_MODE     OPT_MODE_DEVICE
 #endif
 
-// Enable Device stack
-#define CFG_TUD_ENABLED 1
+#ifndef CFG_TUSB_OS
+#define CFG_TUSB_OS                 OPT_OS_PICO
+#endif
 
-// Default is max speed that hardware controller could support with on-chip PHY
-#define CFG_TUD_MAX_SPEED BOARD_TUD_MAX_SPEED
+// CFG_TUSB_DEBUG is defined by compiler in DEBUG build
+// #define CFG_TUSB_DEBUG           0
 
-/* USB DMA on some MCUs can only access a specific SRAM region with restriction
- * on alignment. Tinyusb use follows macros to declare transferring memory so
- * that they can be put into those specific section. e.g
+/* USB DMA on some MCUs can only access a specific SRAM region with restriction on alignment.
+ * Tinyusb use follows macros to declare transferring memory so that they can be put
+ * into those specific section.
+ * e.g
  * - CFG_TUSB_MEM SECTION : __attribute__ (( section(".usb_ram") ))
  * - CFG_TUSB_MEM_ALIGN   : __attribute__ ((aligned(4)))
  */
@@ -74,7 +65,7 @@ extern "C" {
 #endif
 
 #ifndef CFG_TUSB_MEM_ALIGN
-#define CFG_TUSB_MEM_ALIGN __attribute__((aligned(4)))
+#define CFG_TUSB_MEM_ALIGN          __attribute__ ((aligned(4)))
 #endif
 
 //--------------------------------------------------------------------
@@ -82,39 +73,19 @@ extern "C" {
 //--------------------------------------------------------------------
 
 #ifndef CFG_TUD_ENDPOINT0_SIZE
-#define CFG_TUD_ENDPOINT0_SIZE 64
+#define CFG_TUD_ENDPOINT0_SIZE    64
 #endif
 
 //------------- CLASS -------------//
-#define CFG_TUD_HID 1
-#define CFG_TUD_CDC 0
-#define CFG_TUD_MSC 0
-#define CFG_TUD_MIDI 0
-#define CFG_TUD_VENDOR 0
+#define CFG_TUD_CDC             0
+#define CFG_TUD_MSC             0
+#define CFG_TUD_HID             0 
+#define CFG_TUD_MIDI            1 
+#define CFG_TUD_VENDOR          0
 
-// HID buffer size Should be sufficient to hold ID (if any) + Data
-#define CFG_TUD_HID_EP_BUFSIZE 64
-
-#ifndef BOARD_DEVICE_RHPORT_NUM
-#define BOARD_DEVICE_RHPORT_NUM 0
-#endif
-
-#if BOARD_DEVICE_RHPORT_NUM == 0
-#define CFG_TUSB_RHPORT0_MODE (OPT_MODE_DEVICE | BOARD_DEVICE_RHPORT_SPEED)
-#endif
-
-#ifndef CFG_TUSB_RHPORT1_MODE
-#define CFG_TUSB_RHPORT1_MODE OPT_MODE_NONE
-#endif
-
-#if CFG_TUD_CDC
-  #ifndef CFG_TUD_CDC_RX_BUFSIZE
-    #define CFG_TUD_CDC_RX_BUFSIZE  (TUD_OPT_HIGH_SPEED ? 512 : 64)
-  #endif
-  #ifndef CFG_TUD_CDC_TX_BUFSIZE
-    #define CFG_TUD_CDC_TX_BUFSIZE  (TUD_OPT_HIGH_SPEED ? 512 : 64)
-  #endif
-#endif
+// MIDI FIFO size of TX and RX
+#define CFG_TUD_MIDI_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_MIDI_TX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
 
 #ifdef __cplusplus
 }
