4f8c7ab0e0985ed2f7f3fce0c0e45a6d74aca0c6
Dele Olajide <dele@4ng.net>
2025-09-02 19:19:35 +0100

    revert usb_decriptors

--- usb_descriptors.c
+++ usb_descriptors.c
@@ -1,131 +1,177 @@
-// SPDX-License-Identifier: MIT
-/*
- * Copyright (c) 2021 Álvaro Fernández Rojas <noltari@gmail.com>
+/* 
+ * The MIT License (MIT)
  *
- * This file is based on a file originally part of the
- * MicroPython project, http://micropython.org/
+ * Copyright (c) 2019 Ha Thach (tinyusb.org)
  *
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- * Copyright (c) 2019 Damien P. George
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  */
 
-#include <hardware/flash.h>
-#include <tusb.h>
+#include "tusb.h"
 
-#define DESC_STR_MAX 20
+/* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
+ * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
+ *
+ * Auto ProductID layout's Bitmap:
+ *   [MSB]       MIDI | HID | MSC | CDC          [LSB]
+ */
+#define _PID_MAP(itf, n)  ( (CFG_TUD_##itf) << (n) )
+#define USB_PID           (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
+                           _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) )
 
-#define USBD_VID 0x2E8A /* Raspberry Pi */
-#define USBD_PID 0x000A /* Raspberry Pi Pico SDK CDC */
+//--------------------------------------------------------------------+
+// Device Descriptors
+//--------------------------------------------------------------------+
+tusb_desc_device_t const desc_device =
+{
+    .bLength            = sizeof(tusb_desc_device_t),
+    .bDescriptorType    = TUSB_DESC_DEVICE,
+    .bcdUSB             = 0x0200,
+    .bDeviceClass       = 0x00,
+    .bDeviceSubClass    = 0x00,
+    .bDeviceProtocol    = 0x00,
+    .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
 
-#define USBD_DESC_LEN (TUD_CONFIG_DESC_LEN + TUD_CDC_DESC_LEN * CFG_TUD_CDC)
-#define USBD_MAX_POWER_MA 500
+    .idVendor           = 0xCafe,
+    .idProduct          = USB_PID,
+    .bcdDevice          = 0x0100,
 
-#define USBD_ITF_CDC_0 0
-#define USBD_ITF_CDC_1 2
-#define USBD_ITF_MAX 4
+    .iManufacturer      = 0x01,
+    .iProduct           = 0x02,
+    .iSerialNumber      = 0x03,
 
-#define USBD_CDC_0_EP_CMD 0x81
-#define USBD_CDC_1_EP_CMD 0x83
-
-#define USBD_CDC_0_EP_OUT 0x01
-#define USBD_CDC_1_EP_OUT 0x03
-
-#define USBD_CDC_0_EP_IN 0x82
-#define USBD_CDC_1_EP_IN 0x84
-
-#define USBD_CDC_CMD_MAX_SIZE 8
-#define USBD_CDC_IN_OUT_MAX_SIZE 64
-
-#define USBD_STR_0 0x00
-#define USBD_STR_MANUF 0x01
-#define USBD_STR_PRODUCT 0x02
-#define USBD_STR_SERIAL 0x03
-#define USBD_STR_SERIAL_LEN 17
-#define USBD_STR_CDC 0x04
-
-static const tusb_desc_device_t usbd_desc_device = {
-		.bLength = sizeof(tusb_desc_device_t),
-		.bDescriptorType = TUSB_DESC_DEVICE,
-		.bcdUSB = 0x0200,
-		.bDeviceClass = TUSB_CLASS_MISC,
-		.bDeviceSubClass = MISC_SUBCLASS_COMMON,
-		.bDeviceProtocol = MISC_PROTOCOL_IAD,
-		.bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
-		.idVendor = USBD_VID,
-		.idProduct = USBD_PID,
-		.bcdDevice = 0x0100,
-		.iManufacturer = USBD_STR_MANUF,
-		.iProduct = USBD_STR_PRODUCT,
-		.iSerialNumber = USBD_STR_SERIAL,
-		.bNumConfigurations = 1,
+    .bNumConfigurations = 0x01
 };
 
-static const uint8_t usbd_desc_cfg[USBD_DESC_LEN] = {
-		TUD_CONFIG_DESCRIPTOR(1, USBD_ITF_MAX, USBD_STR_0, USBD_DESC_LEN,
-													TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, USBD_MAX_POWER_MA),
+// Invoked when received GET DEVICE DESCRIPTOR
+// Application return pointer to descriptor
+uint8_t const * tud_descriptor_device_cb(void)
+{
+  return (uint8_t const *) &desc_device;
+}
 
-		TUD_CDC_DESCRIPTOR(USBD_ITF_CDC_0, USBD_STR_CDC, USBD_CDC_0_EP_CMD,
-											 USBD_CDC_CMD_MAX_SIZE, USBD_CDC_0_EP_OUT, USBD_CDC_0_EP_IN,
-											 USBD_CDC_IN_OUT_MAX_SIZE),
 
-		TUD_CDC_DESCRIPTOR(USBD_ITF_CDC_1, USBD_STR_CDC, USBD_CDC_1_EP_CMD,
-											 USBD_CDC_CMD_MAX_SIZE, USBD_CDC_1_EP_OUT, USBD_CDC_1_EP_IN,
-											 USBD_CDC_IN_OUT_MAX_SIZE),
+//--------------------------------------------------------------------+
+// Configuration Descriptor
+//--------------------------------------------------------------------+
+
+enum
+{
+  ITF_NUM_MIDI = 0,
+  ITF_NUM_MIDI_STREAMING,
+  ITF_NUM_TOTAL
 };
 
-static char usbd_serial[USBD_STR_SERIAL_LEN] = "000000000000";
+#define CONFIG_TOTAL_LEN  (TUD_CONFIG_DESC_LEN + TUD_MIDI_DESC_LEN)
 
-static const char *const usbd_desc_str[] = {
-		[USBD_STR_MANUF] = "Raspberry Pi",
-		[USBD_STR_PRODUCT] = "Pico",
-		[USBD_STR_SERIAL] = usbd_serial,
-		[USBD_STR_CDC] = "Board CDC",
+#if CFG_TUSB_MCU == OPT_MCU_LPC175X_6X || CFG_TUSB_MCU == OPT_MCU_LPC177X_8X || CFG_TUSB_MCU == OPT_MCU_LPC40XX
+  // LPC 17xx and 40xx endpoint type (bulk/interrupt/iso) are fixed by its number
+  // 0 control, 1 In, 2 Bulk, 3 Iso, 4 In etc ...
+  #define EPNUM_MIDI   0x02
+#else
+  #define EPNUM_MIDI   0x01
+#endif
+
+uint8_t const desc_fs_configuration[] =
+{
+  // Config number, interface count, string index, total length, attribute, power in mA
+  TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
+
+  // Interface number, string index, EP Out & EP In address, EP size
+  TUD_MIDI_DESCRIPTOR(ITF_NUM_MIDI, 0, EPNUM_MIDI, 0x80 | EPNUM_MIDI, 64)
 };
 
-const uint8_t *tud_descriptor_device_cb(void)
+#if TUD_OPT_HIGH_SPEED
+uint8_t const desc_hs_configuration[] =
 {
-	return (const uint8_t *)&usbd_desc_device;
+  // Config number, interface count, string index, total length, attribute, power in mA
+  TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
+
+  // Interface number, string index, EP Out & EP In address, EP size
+  TUD_MIDI_DESCRIPTOR(ITF_NUM_MIDI, 0, EPNUM_MIDI, 0x80 | EPNUM_MIDI, 512)
+};
+#endif
+
+// Invoked when received GET CONFIGURATION DESCRIPTOR
+// Application return pointer to descriptor
+// Descriptor contents must exist long enough for transfer to complete
+uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
+{
+  (void) index; // for multiple configurations
+
+#if TUD_OPT_HIGH_SPEED
+  // Although we are highspeed, host may be fullspeed.
+  return (tud_speed_get() == TUSB_SPEED_HIGH) ?  desc_hs_configuration : desc_fs_configuration;
+#else
+  return desc_fs_configuration;
+#endif
 }
 
-const uint8_t *tud_descriptor_configuration_cb(uint8_t index)
+//--------------------------------------------------------------------+
+// String Descriptors
+//--------------------------------------------------------------------+
+
+// array of pointer to string descriptors
+char const* string_desc_arr [] =
 {
-	return usbd_desc_cfg;
-}
+  (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
+  "Inspired Futures Ltd",        // 1: Manufacturer
+  "Orinayo Device",              // 2: Product
+  NULL,                      	 // 3: Serials, should use chip ID
+};
 
-const uint16_t *tud_descriptor_string_cb(uint8_t index, uint16_t langid)
+static uint16_t _desc_str[32];
+
+// Invoked when received GET STRING DESCRIPTOR request
+// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
+uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
 {
-	static uint16_t desc_str[DESC_STR_MAX];
-	uint8_t len;
+  (void) langid;
 
-	if (index == 0)
-	{
-		desc_str[1] = 0x0409;
-		len = 1;
-	}
-	else
-	{
-		const char *str;
-		char serial[USBD_STR_SERIAL_LEN];
+  uint8_t chr_count;
 
-		if (index >= sizeof(usbd_desc_str) / sizeof(usbd_desc_str[0]))
-			return NULL;
+  if ( index == 0)
+  {
+    memcpy(&_desc_str[1], string_desc_arr[0], 2);
+    chr_count = 1;
+  }else
+  {
+    // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
+    // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
 
-		str = usbd_desc_str[index];
-		for (len = 0; len < DESC_STR_MAX - 1 && str[len]; ++len)
-			desc_str[1 + len] = str[len];
-	}
+    if ( !(index < sizeof(string_desc_arr)/sizeof(string_desc_arr[0])) ) return NULL;
 
-	desc_str[0] = (TUSB_DESC_STRING << 8) | (2 * len + 2);
+    const char* str = string_desc_arr[index];
 
-	return desc_str;
-}
+    // Cap at max char
+    chr_count = strlen(str);
+    if ( chr_count > 31 ) chr_count = 31;
 
-void usbd_serial_init(void)
-{
-	uint8_t id[8];
+    // Convert ASCII string into UTF-16
+    for(uint8_t i=0; i<chr_count; i++)
+    {
+      _desc_str[1+i] = str[i];
+    }
+  }
 
-	flash_get_unique_id(id);
+  // first byte is length (including header), second byte is string type
+  _desc_str[0] = (TUSB_DESC_STRING << 8 ) | (2*chr_count + 2);
 
-	snprintf(usbd_serial, USBD_STR_SERIAL_LEN, "%02X%02X%02X%02X%02X%02X%02X%02X",
-					 id[0], id[1], id[2], id[3], id[4], id[5], id[6], id[7]);
+  return _desc_str;
 }
