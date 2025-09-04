30351e0b2ae59ebd29518497a5d31c77140cb979
Dele Olajide <dele@4ng.net>
2025-09-04 19:08:13 +0100

    fix declaration error

--- pico_bluetooth.c
+++ pico_bluetooth.c
@@ -23,7 +23,7 @@ void midi_play_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3);
 void midi_play_slash_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3, uint8_t p4);
 void midi_ketron_arr(uint8_t code, bool on);
 void midi_ketron_footsw(uint8_t code, bool on);
-void play_chord(uint8_t base, uint8_t green, uint8_t red, uint8_t yellow, uint8_t blue, uint8_t orange);
+void play_chord(bool on, uint8_t base, uint8_t green, uint8_t red, uint8_t yellow, uint8_t blue, uint8_t orange);
 
 // Platform Overrides
 static void pico_bluetooth_init(int argc, const char** argv) {
