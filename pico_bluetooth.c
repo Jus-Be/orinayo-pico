a5b44e47dc40333a3103f6cbf1a780a61e53c20e
Dele Olajide <dele@4ng.net>
2025-09-05 23:55:40 +0100

    version 0.9.2

--- pico_bluetooth.c
+++ pico_bluetooth.c
@@ -716,7 +716,7 @@ void play_chord(bool on, bool up, uint8_t base, uint8_t green, uint8_t red, uint
 			seq_index++;	
 			if (seq_index > 11) seq_index = 0;	
 		} else {
-			note = bass_note ? bass_note + 12 + (pos * 12) : chord_note;
+			note = (bass_note ? bass_note : chord_note) + 12 + (pos * 12);
 			old_midinotes[0] = note;
 			midi_send_note(0x90, note, velocity);
 		}			
