91d7feea625c0ec364c404106e3515bbbb765b5d
Dele Olajide <dele@4ng.net>
2025-09-18 22:11:09 +0100

    more debugging

--- main.c
+++ main.c
@@ -222,8 +222,8 @@ void midi_play_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3)
 			
 			if (enable_ample_guitar) {	// squeeze into C1 - B2
 				p1 = (p1 % 12) + 36;
-				p2 = (p2 % 12) + ((p1 % 12 > 6) ? 36 : 48);
-				p3 = (p3 % 12) + ((p1 % 12 > 6) ? 36 : 48);;
+				p2 = (p2 % 12) + ((p2  % 12) <  (p1 % 12) ? 48 : 36);
+				p3 = (p3 % 12) + ((p3  % 12) <  (p1 % 12) ? 48 : 36);
 			}
 			
 			midi_send_note(0x93, p1, 32);
@@ -251,9 +251,9 @@ void midi_play_slash_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3, uint8_t 
 
 			if (enable_ample_guitar) {	// squeeze into C1 - B2
 				p1 = (p1 % 12) + 36;
-				p2 = (p2 % 12) + ((p1 % 12 > 6) ? 36 : 48);
-				p3 = (p3 % 12) + ((p1 % 12 > 6) ? 36 : 48);
-				p4 = (p4 % 12) + ((p1 % 12 > 6) ? 36 : 48);			
+				p2 = (p2 % 12) + ((p2  % 12) <  (p1 % 12) ? 48 : 36);
+				p3 = (p3 % 12) + ((p3  % 12) <  (p2 % 12) ? 48 : 36);
+				p4 = (p4 % 12) + ((p4  % 12) <  (p2 % 12) ? 48 : 36);
 			}
 			
 			midi_send_note(0x93, p1, 32);
