31ac9d5271bb55f4eeb0ef5b7856793c8ed7c4b4
Dele Olajide <dele@4ng.net>
2025-09-23 15:46:29 +0100

    debug ble -2

--- bluepad32/bt/uni_bt_le.c
+++ bluepad32/bt/uni_bt_le.c
@@ -1290,6 +1290,7 @@ void uni_bt_le_on_gap_event_advertising_report(const uint8_t* packet, uint16_t s
 	}
 
     if (name[0] == 'O' && name[1] == 'r' && name[2] == 'i' && name[3] == 'n' && name[4] == 'a' && name[5] == 'y' && name[6] == 'o') {
+		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false); 
 		orinayo_enabled = true;
 		hog_connect(addr, addr_type);		
 		return;	
