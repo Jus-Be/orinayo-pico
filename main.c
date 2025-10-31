514c33fc37a06b133e7be58c49148ea0ee82c10c
Dele Olajide <dele@4ng.net>
2025-10-31 15:36:08 +0000

    fix sampler

--- main.c
+++ main.c
@@ -485,8 +485,9 @@ void midi_play_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3) {
 			old_p2 = p2;
 			old_p3 = p3;			
 
-			if (enable_seqtrak) {
-				if (seqtrak_chord > 0) midi_send_note(0x9A, seqtrak_chord + 47, 127);	// start seqtrak playing sample
+			if (enable_seqtrak && seqtrak_chord > 0) {
+				uint8_t msg[3] = {0x9A, seqtrak_chord + 59, 127};
+				tud_midi_n_stream_write(0, 0, msg, 3);
 			}			
 			
 		} else {
@@ -495,8 +496,9 @@ void midi_play_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3) {
 			midi_send_chord_note( old_p3, 0);
 			midi_send_chord_note( old_p4, 0);
 
-			if (enable_seqtrak) {
-				if (seqtrak_chord > 0) midi_send_note(0x8A, seqtrak_chord + 47, 0);	// stop seqtrak playing sample
+			if (enable_seqtrak && seqtrak_chord > 0) {
+				uint8_t msg[3] = {0x8A, seqtrak_chord + 59, 0};
+				tud_midi_n_stream_write(0, 0, msg, 3);
 			}			
 		}
 	}
@@ -525,8 +527,9 @@ void midi_play_slash_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3, uint8_t 
 			old_p3 = p3;		
 			old_p4 = p4;
 
-			if (enable_seqtrak) {
-				if (seqtrak_chord > 0) midi_send_note(0x9A, seqtrak_chord + 59, 127);	// start seqtrak playing sample
+			if (enable_seqtrak && seqtrak_chord > 0) {
+				uint8_t msg[3] = {0x9A, seqtrak_chord + 59, 127};
+				tud_midi_n_stream_write(0, 0, msg, 3);
 			}
 			
 		} else {
@@ -535,8 +538,9 @@ void midi_play_slash_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3, uint8_t 
 			midi_send_chord_note( old_p3, 0);		
 			midi_send_chord_note( old_p4, 0);	
 
-			if (enable_seqtrak) {
-				if (seqtrak_chord > 0) midi_send_note(0x8A, seqtrak_chord + 59, 0);	// stop seqtrak playing sample
+			if (enable_seqtrak && seqtrak_chord > 0) {
+				uint8_t msg[3] = {0x8A, seqtrak_chord + 59, 0};
+				tud_midi_n_stream_write(0, 0, msg, 3);
 			}			
 		}
 	}
