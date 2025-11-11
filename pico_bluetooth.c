5546a59b728f51425267f9cf2c4f2063028c8909
Dele Olajide <dele@4ng.net>
2025-11-11 23:40:22 +0000

    use 62

--- pico_bluetooth.c
+++ pico_bluetooth.c
@@ -866,7 +866,7 @@ static void pico_bluetooth_on_controller_data(uni_hid_device_t* d, uni_controlle
 			if (enable_modx) 
 			{
 				if (mbut2) {
-					midi_send_control_change(0xB0, 0, 64); 				// MSB 64						
+					midi_send_control_change(0xB0, 0, 62); 				// MSB 62						
 					midi_send_control_change(0xB0, 32, 0); 				// LSB 0 Page 1
 					midi_send_program_change(0xC0, style_group % 16);	// set PC to performance/set list no					
 				}
