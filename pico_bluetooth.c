813c903914c5ad466bd5d05df409c9b226ee7713
Dele Olajide <dele@4ng.net>
2025-11-12 19:08:48 +0000

    send MSB/LSB last

--- pico_bluetooth.c
+++ pico_bluetooth.c
@@ -866,9 +866,9 @@ static void pico_bluetooth_on_controller_data(uni_hid_device_t* d, uni_controlle
 			if (enable_modx) 
 			{
 				if (mbut2) {
+					midi_send_program_change(0xC3, style_group % 16);	// set PC to performance/set list no						
+					midi_send_control_change(0xB3, 32, 0); 				// LSB 0 Page 1	
 					midi_send_control_change(0xB3, 0, 62); 				// MSB 62						
-					midi_send_control_change(0xB3, 32, 0); 				// LSB 0 Page 1
-					midi_send_program_change(0xC3, style_group % 16);	// set PC to performance/set list no					
 				}
 			}			
 				
