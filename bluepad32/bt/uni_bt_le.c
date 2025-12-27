839d8a7282e31e7929af5a8591f283980d470670
Dele Olajide <dele@4ng.net>
2025-12-27 00:16:51 +0000

    build 1216

--- bluepad32/bt/uni_bt_le.c
+++ bluepad32/bt/uni_bt_le.c
@@ -774,11 +774,11 @@ void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint8_t *pa
 				
 			if (liberlive_enabled) {			
 				// Write Chord Key Mapping			
-				static uint8_t chord_mappings[26] = {177, 30, 31, 21, 0, 128, 147, 117, 5, 85, 81, 113, 160, 145, 112, 0, 80, 33, 65, 176, 144, 112, 0, 48, 32, 64};
-				gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 26, chord_mappings);
+				//static uint8_t chord_mappings[26] = {177, 30, 31, 21, 0, 128, 147, 117, 5, 85, 81, 113, 160, 145, 112, 0, 80, 33, 65, 176, 144, 112, 0, 48, 32, 64};
+				//gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 26, chord_mappings);
 
-				//static uint8_t set_drum[10] = {177, 30, 23, 5, 0, 1, 42, 28, 1, 1};
-				//gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 10, set_drum);
+				static uint8_t set_drum[10] = {177, 30, 23, 5, 0, 42, 28, 0, 1, 1};
+				gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 10, set_drum);
 				query_state = 2;
 			}
 			else
