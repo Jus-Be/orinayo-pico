4971c633f450ffa82828ccd27d708f70eb154d10
Dele Olajide <dele@4ng.net>
2025-12-27 00:30:33 +0000

    build again

--- bluepad32/bt/uni_bt_le.c
+++ bluepad32/bt/uni_bt_le.c
@@ -777,8 +777,12 @@ void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint8_t *pa
 				//static uint8_t chord_mappings[26] = {177, 30, 31, 21, 0, 128, 147, 117, 5, 85, 81, 113, 160, 145, 112, 0, 80, 33, 65, 176, 144, 112, 0, 48, 32, 64};
 				//gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 26, chord_mappings);
 
-				static uint8_t set_drum[10] = {177, 30, 23, 5, 0, 42, 28, 0, 1, 1};
-				gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 10, set_drum);
+				//static uint8_t set_drum[10] = {177, 30, 23, 5, 0, 42, 28, 0, 1, 1};
+				//gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 10, set_drum);
+
+				static uint8_t set_chord[10] = {177, 30, 22, 5, 0, 32, 2, 0, 1, 1};
+				gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 10, set_chord);
+
 				query_state = 2;
 			}
 			else
@@ -795,11 +799,11 @@ void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint8_t *pa
 			cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true); 	
 
 			if (liberlive_enabled) {			
-				static uint8_t drum_preview[6] = {177, 30, 38, 1, 0, 1};			
-				gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 6, drum_preview);
+				//static uint8_t drum_preview[6] = {177, 30, 38, 1, 0, 1};			
+				//gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 6, drum_preview);
 
-				//static uint8_t set_tip[8] = {177, 30, 12, 3, 0, 0, 0, 0};			
-				//gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 8, set_tip);
+				static uint8_t chord_preview[6] = {177, 30, 37, 1, 0, 1};			
+				gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 6, chord_preview);
 
 				query_state = 3;
 			}				
