16ce63d57cca0d180cf529e79224f82af9674cc0
Dele Olajide <dele@4ng.net>
2025-12-30 08:23:02 +0000

    build 0822

--- bluepad32/bt/uni_bt_le.c
+++ bluepad32/bt/uni_bt_le.c
@@ -808,7 +808,7 @@ void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint8_t *pa
 		if (query_state == 2) 	{
 
 			if (liberlive_enabled) {
-				uint8_t set_chord[10] = {177, 30, 22, 5, 0, 0, 12, 7, 1, 1};	// chord paddle, group, item, key, difficulty
+				uint8_t set_chord[10] = {177, 30, 22, 5, 0, 0, 4, 12, 1, 1};	// chord paddle, group, item, key, difficulty
 				gatt_client_write_value_of_characteristic(handle_gatt_client_event, connection_handle, server_characteristic.value_handle, 10, set_chord);												
 				query_state = 3;
 			}				
