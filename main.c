da410d7c4153595b5141b00dad61cb01289bcba6
Dele Olajide <dele@4ng.net>
2026-02-21 21:42:51 +0000

    build 2142

--- main.c
+++ main.c
@@ -155,10 +155,8 @@ int main() {
 	
 	//m5audio_set_play_mode(M5AUDIO_PLAY_MODE_SINGLE_LOOP);		
 
-	
-	m5audio_repeat_at_time(0, 0, 0, 8);
-	sleep_ms(1000);	
 	m5audio_play();	
+	m5audio_repeat_at_time(0, 0, 0, 8);	
 	
     while (true) {
 		tud_task(); // tinyusb device task
