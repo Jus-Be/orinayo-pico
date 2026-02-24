5de8f00d85806fd2edca36426bd53dae2c236977
Dele Olajide <dele@4ng.net>
2026-02-24 22:02:35 +0000

    build 2202

--- main.c
+++ main.c
@@ -647,7 +647,7 @@ void midi_play_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3) {
 			}
 			else
 				
-			if (enable_midi_drums && basic_chord > 0 && (old_p1 != p1 || old_p2 != p2 || old_p3 != p3)) {
+			if (enable_midi_drums && basic_chord > 0)) {
 				m5audio_play_track(basic_chord);
 			}
 			
@@ -669,7 +669,7 @@ void midi_play_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3) {
 			else
 				
 			if (enable_midi_drums) {
-				m5audio_stop();
+				//m5audio_stop();
 			}			
 		}
 	}
@@ -699,7 +699,7 @@ void midi_play_slash_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3, uint8_t 
 			}
 			else
 				
-			if (enable_midi_drums && basic_chord > 0 && (old_p1 != p1 || old_p2 != p2 || old_p3 != p3 || old_p4 != p4)) {
+			if (enable_midi_drums && basic_chord > 0) {
 				m5audio_play_track(basic_chord);
 			}	
 
@@ -722,7 +722,7 @@ void midi_play_slash_chord(bool on, uint8_t p1, uint8_t p2, uint8_t p3, uint8_t 
 			else
 				
 			if (enable_midi_drums) {
-				m5audio_stop();
+				//m5audio_stop();
 			}			
 		}
 	}
