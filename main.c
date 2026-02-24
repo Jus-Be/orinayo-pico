dd9018db2a0795552a236b6b65b2edd55d233cc6
Dele Olajide <dele@4ng.net>
2026-02-24 21:04:03 +0000

    build 2103

--- main.c
+++ main.c
@@ -153,13 +153,13 @@ int main() {
 
 	// setup UART1 
 	m5audio_init();
-	//m5audio_set_play_mode(M5AUDIO_PLAY_MODE_SINGLE_STOP);
-	//m5audio_play_track(4);	
+	m5audio_set_play_mode(M5AUDIO_PLAY_MODE_SINGLE_STOP);
+	m5audio_play_track(5);	
 	
-	m5audio_select_audio_num(5);
+	//m5audio_select_audio_num(5);
 	//m5audio_repeat_at_time(0, 0, 0, 9);		
-	m5audio_set_play_mode(M5AUDIO_PLAY_MODE_SINGLE_LOOP);		
-	m5audio_play();
+	//m5audio_set_play_mode(M5AUDIO_PLAY_MODE_SINGLE_LOOP);		
+	//m5audio_play();
 
     while (true) {
 		tud_task(); // tinyusb device task
