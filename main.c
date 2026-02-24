5f2559303394e1f523e2bccc81cc7ec5c30fd0a9
Dele Olajide <dele@4ng.net>
2026-02-24 20:01:44 +0000

    build 2001

--- main.c
+++ main.c
@@ -153,7 +153,8 @@ int main() {
 
 	// setup UART1 
 	m5audio_init();
-	m5audio_set_play_mode(M5AUDIO_PLAY_MODE_SINGLE_STOP);		
+	m5audio_set_play_mode(M5AUDIO_PLAY_MODE_SINGLE_STOP);
+	m5audio_play_track(1);	
 	
 	//m5audio_select_audio_num(5);
 	//m5audio_repeat_at_time(0, 0, 0, 9);		
