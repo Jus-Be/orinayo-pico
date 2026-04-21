8017d4011516d83da3fec8a03f3c0cb604a37608
Dele Olajide <dele@4ng.net>
2026-04-21 09:28:49 +0100

    build 0928

--- tusb_config.h
+++ tusb_config.h
@@ -118,7 +118,7 @@ extern "C" {
 #define CFG_TUH_ENUMERATION_BUFSIZE 256
 
 // Host class enablement.
-#define CFG_TUH_HUB                 1
+#define CFG_TUH_HUB                 0
 #define CFG_TUH_DEVICE_MAX          (3 * CFG_TUH_HUB + 1)
 #define CFG_TUH_MIDI                1
 #define CFG_TUH_MIDI_STREAM_API		1
