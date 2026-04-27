5906561460c679a899ebd0c671ec462311692c6e
Dele Olajide <dele@4ng.net>
2026-04-27 17:41:59 +0100

    build 1741

--- main.c
+++ main.c
@@ -567,7 +567,7 @@ void tuh_midi_rx_cb(uint8_t idx, uint32_t xferred_bytes) {
 								}
 								else
 									
-								if (cc_value == 0x16) {							// previous style
+								if (cc_value == 0x7F) {							// previous style
 									dpad_down = 1; starpower = 0; orange = 0; but4 = 1;
 									midi_bluetooth_handle_data();								
 								}
