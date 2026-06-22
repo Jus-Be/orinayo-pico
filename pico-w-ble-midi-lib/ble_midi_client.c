f6fb0ed6889a2515f573c9b605bc2e0995f3156d
Dele Olajide <dele@4ng.net>
2026-06-22 19:09:03 +0100

    build 1908

--- pico-w-ble-midi-lib/ble_midi_client.c
+++ pico-w-ble-midi-lib/ble_midi_client.c
@@ -575,9 +575,7 @@ static void enter_client_mode()
     // If your server can handled a shorter connection interval, please have it request
     // to update the parameters to the shorter interval
     
-	//gap_set_connection_parameters(96, 48, 6, 12, 0, 1000, 0x01, 6 * 2);
-	//gap_set_connection_parameters(96, 48, 12, 24, 0, 200,  0x0000, 0xffff);
-	gap_set_connection_parameters(96, 48, 24, 48, 0, 100, 2, 64);	
+	gap_set_connection_parameters(96, 48, 24, 48, 0, 100, 0x0000, 0xffff);	
 }
 
 void ble_midi_client_init(const char* profile_name, uint8_t profile_name_len, io_capability_t iocaps_, uint8_t secmask_)
